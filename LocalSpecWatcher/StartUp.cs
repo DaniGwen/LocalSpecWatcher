9f91e76f5fd2f9948c04440f63305b1aca35da33
Daniel <dani.gwenski@users.noreply.github.com>
2020-10-27 14:54:49 +0200

    Update

--- LocalSpecWatcher/StartUp.cs
+++ LocalSpecWatcher/StartUp.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Management;
-using System.Linq;
 
 namespace LocalSpecWatcher
 {
@@ -8,154 +6,17 @@ namespace LocalSpecWatcher
     {
         static void Main()
         {
-            GetProcessorInfo();
-            GetPartitionInfo();
-            GetDynamicMemoryInfo();
-            GetBatteryInfo();
-            GetGpuInfo();
-        }
+            var systemInfo = new SystemInfo();
 
-        private static void GetProcessorInfo()
-        {
-            string processor = "Win32_Processor";
-
-            ManagementObjectSearcher processorObj = new ManagementObjectSearcher("select * from " + processor);
-
-            Console.WriteLine("==========");
-            Console.WriteLine("Processor:");
-            Console.WriteLine("==========");
-
-            foreach (ManagementObject obj in processorObj.Get())
-            {
-                foreach (PropertyData property in obj.Properties)
-                {
-                    var propertyName = property.Name;
-
-                    if (propertyName.StartsWith("Name") ||
-                        propertyName.Contains("NumberOfCores") ||
-                        propertyName.Contains("CurrentClockSpeed") ||
-                        propertyName.Contains("MaxClockSpeed"))
-                    {
-                        Console.WriteLine($"{property.Name}:  {property.Value}");
-                    }
-                }
-            }
+            Console.WriteLine("OS: " + systemInfo.OperatingSystem);
             Console.WriteLine();
-        }
-
-        private static void GetDynamicMemoryInfo()
-        {
-            string physicalMemory = "Win32_PhysicalMemory";
-
-            ManagementObjectSearcher physicalMemoryObj = new ManagementObjectSearcher("select * from " + physicalMemory);
-
-            Console.WriteLine("================");
-            Console.WriteLine("Physical Memory:");
-            Console.WriteLine("================");
-
-            foreach (ManagementObject obj in physicalMemoryObj.Get())
-            {
-                foreach (PropertyData property in obj.Properties)
-                {
-                    var propertyName = property.Name;
-
-                    if (propertyName.Contains("BankLabel") ||
-                        propertyName.StartsWith("Capacity") ||
-                        propertyName.StartsWith("Speed"))
-                    {
-                        Console.WriteLine($"{property.Name}: {property.Value}");
-                    }
-                }
-            }
+            Console.WriteLine("Hostname: " + systemInfo.Hostname);
             Console.WriteLine();
-        }
-
-        private static void GetPartitionInfo()
-        {
-            string diskPartition = "Win32_DiskDrive";
-
-            ManagementObjectSearcher partitionObj = new ManagementObjectSearcher("select * from " + diskPartition);
-
-            Console.WriteLine("============");
-            Console.WriteLine("Disk drives:");
-            Console.WriteLine("============");
-
-            foreach (ManagementObject obj in partitionObj.Get())
-            {
-                foreach (PropertyData property in obj.Properties)
-                {
-                    var propertyName = property.Name;
-
-                    if (propertyName.StartsWith("Model") ||
-                        propertyName.StartsWith("Size") ||
-                        propertyName.StartsWith("SerialNumber") ||
-                        propertyName.StartsWith("Partitions"))
-                    {
-                        Console.WriteLine($"{property.Name}:  {property.Value}");
-                    }
-                }
-            }
+            Console.WriteLine("Ram: " + systemInfo.Ram);
             Console.WriteLine();
-        }
-
-        private static void GetBatteryInfo()
-        {
-            string battery = "Win32_Battery";
-
-            ManagementObjectSearcher batteryObj = new ManagementObjectSearcher("select * from " + battery);
-
-            Console.WriteLine("========");
-            Console.WriteLine("Battery:");
-            Console.WriteLine("========");
-
-            foreach (ManagementObject obj in batteryObj.Get())
-            {
-                foreach (PropertyData property in obj.Properties)
-                {
-                    var propertyName = property.Name;
-
-                    if (propertyName == "StatusInfo")
-                    {
-                        continue;
-                    }
-
-                    if (propertyName.StartsWith("Description") ||
-                        propertyName.StartsWith("Name") ||
-                        propertyName.Contains("EstimatedChargeRemaining") ||
-                        propertyName.Contains("EstimatedRunTime") ||
-                        propertyName.StartsWith("Status"))
-                    {
-                        Console.WriteLine(propertyName == "Name" ? $"Model: {property.Value}" : $"{property.Name}: {property.Value}");
-                    }
-                }
-            }
+            Console.WriteLine("Processor: " +  systemInfo.Processor);            
             Console.WriteLine();
-        }
-
-        private static void GetGpuInfo()
-        {
-            string gpu = "Win32_DisplayControllerConfiguration";
-
-            ManagementObjectSearcher gpuObj = new ManagementObjectSearcher("select * from " + gpu);
-
-            Console.WriteLine("==============");
-            Console.WriteLine("Video adapter:");
-            Console.WriteLine("==============");
-                         
-            foreach (ManagementObject obj in gpuObj.Get())
-            {
-                foreach (PropertyData property in obj.Properties)
-                {
-                    var propertyName = property.Name;
-
-                    if (propertyName.StartsWith("Description") ||
-                        propertyName.StartsWith("VideoMode"))
-                    {
-                        Console.WriteLine($"{property.Name}:  {property.Value}");
-                    }
-                }
-            }
-            Console.WriteLine();
+            Console.WriteLine("Drives: " + systemInfo.Drives);
         }
     }
 }
